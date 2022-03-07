1c54ea7130681dcd89df9e29af5211c89540f724
Varshith <87647050+varshith07@users.noreply.github.com>
2022-03-07 18:19:09 +0530

    Add files via upload

---
+++ godady-dynamic-website/dynamic-website/godaddy dynamic/app.js
@@ -0,0 +1,26 @@
+function predict_diabetes()
+{
+    window.location.replace("https://share.streamlit.io/praneethkanchanakuntla/diabets/main/app.py");
+}
+function predict_heart()
+{
+    window.location.replace("https://share.streamlit.io/praneethkanchanakuntla/heartattack/main/app.py")
+}
+function predict_stroke()
+{
+    window.location.replace("https://strokediseaseprediction.herokuapp.com/")
+}
+function book()
+{
+    window.location.replace("https://www.practo.com/order")
+}
+function prescribe()
+{
+   window.location.replace("https://www.apollopharmacy.in/")
+}
+function com()
+{
+    let x=document.getElementById("comment").value;
+    document.getElementById("output").innerHTML=x+"  "+'Got them please book the doctors Appointment';
+}
+    
